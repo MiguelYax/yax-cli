61649b491babbad93781ae67e06bbf453abeca6e
Miguel Yax <mig_dj@hotmail.com>
2025-05-30 15:15:25 -0600

    Fix: prompt unexpected behavior.

---
+++ .changeset/dry-timers-shave.md
@@ -0,0 +1,5 @@
+---
+"yax-cli": patch
+---
+
+Fix prompt unexpected behavior. Add support for multiple prompt at the same time.
--- packages/yax-cli/src/prompt.ts
+++ packages/yax-cli/src/prompt.ts
@@ -1,40 +1,58 @@
 import readline from 'readline';
-import { createInterface } from 'readline';
 import { stdin as input, stdout as output } from 'process';
-export class Prompt {
+
+export class PromptReader {
   private rl: readline.Interface;
 
   constructor() {
-    this.rl = createInterface({ input, output });
+    this.rl = readline.createInterface({ input, output });
   }
 
-  async input(question: string): Promise<string> {
+  async input(config: PromptType): Promise<string> {
     return new Promise((resolve) => {
-      this.rl.question(question, (answer) => {
+      this.rl.question(`${config.question}\n`, (answer) => {
+        const value = answer.trim();
+        if (!value) {
+          console.log('Input cannot be empty, please try again.');
+          return this.input(config).then(resolve);
+        }
+        if (config.validate) {
+          const isValid = config.validate(value);
+          if (!isValid) {
+            console.log('Invalid input, please try again.');
+            return this.input(config).then(resolve);
+          }
+        }
         resolve(answer);
       });
     });
   }
 
-  async confirm(question: string): Promise<boolean> {
+  async confirm(config: PromptType): Promise<boolean> {
     return new Promise((resolve) => {
-      this.rl.question(`${question} (y/n) `, (answer) => {
+      this.rl.question(`${config.question}\n(y/n): `, (answer) => {
         const normalizedAnswer = answer.trim().toLowerCase();
-        resolve(normalizedAnswer === 'y' || normalizedAnswer === 'yes');
+        return resolve(normalizedAnswer === 'y' || normalizedAnswer === 'yes');
       });
     });
   }
 
-  async select<T>(question: string, options: T[]): Promise<T> {
+  async select(config: PromptType): Promise<string> {
     return new Promise((resolve) => {
-      const formattedOptions = options.map((option, index) => `${index + 1}. ${option}`).join('\n');
-      this.rl.question(`${question}\n${formattedOptions}\nSelect an option: `, (answer) => {
+      const items = config.options ?? [];
+      const size = items.length;
+      if (size === 0) {
+        return resolve('');
+      }
+
+      const formattedOptions = items.map((option, index) => `${index + 1}. ${option}`).join('\n');
+      this.rl.question(`${config.question}\n${formattedOptions}\nSelect an option: `, (answer) => {
         const index = parseInt(answer.trim(), 10) - 1;
-        if (index >= 0 && index < options.length) {
-          resolve(options[index]);
+        if (index >= 0 && index < size) {
+          resolve(items[index]);
         } else {
           console.log('Invalid selection, please try again.');
-          this.select(question, options).then(resolve);
+          return this.select(config).then(resolve);
         }
       });
     });
@@ -44,3 +62,27 @@ export class Prompt {
     this.rl.close();
   }
 }
+
+export type PromptType = {
+  name: string
+  question: string;
+  type: 'input' | 'confirm' | 'select';
+  validate?: (input: string) => boolean;
+  options?: string[];
+}
+
+export type PromptAnswerType = {
+  [key: string]: string | boolean;
+}
+
+export const prompt = async (questions: PromptType[]) => { 
+  const reader = new PromptReader();
+  const answers : PromptAnswerType = {};
+
+  for (const q of questions) {
+    answers[q.name] = await reader[q.type](q);
+  }
+
+  reader.close();
+  return answers;
+};
